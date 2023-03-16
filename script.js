4cf7f706d4b6ec0e1b2b425f6448cd3d507d4aab
Britain Seright <britainseright@gmail.com>
2023-03-16 00:21:20 -0500

    add border color on invalid

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let firstName = document.querySelector("#first-name");
 let lastName = document.querySelector("#last-name");
 let names = document.querySelectorAll(".name");
 let email = document.querySelector("#email");
+let emailLabel = document.querySelector("#email-label");
+let emailStatus;
 let phone = document.querySelector("#phone");
 let password = document.querySelector("#password");
 let confirmPassword = document.querySelector("#confirm-password");
@@ -19,7 +21,27 @@ phone.onkeypress = function(event) {
     }
 }
 
+function validateEmail() {
+    let valid = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  
+    if (email.value.match(valid)) {
+        if (emailStatus === "invalid") {
+            emailLabel.style.marginLeft = "0px";
+            emailStatus = "valid";
+            emailLabel.textContent = "Email";
+            email.style.borderColor = "";
+        }
+    } else {
+        emailLabel.style.marginLeft = "-5px";
+        emailStatus = "invalid";
+        emailLabel.textContent = "Invalid Email";
+        email.style.borderColor = "rgb(245, 132, 132)";
+    }
+}
+
 submitLink.addEventListener("click", function() {
 
-    form.submit();
+    validateEmail();
+
+    // form.submit();
 });
