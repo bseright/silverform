64a8ded027f5da98ca4d5345fe58c604e6c19828
Britain Seright <51188389+bseright@users.noreply.github.com>
2023-03-15 20:56:22 -0500

    Allow only alpha keypress in name fields

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-let submitLink = document.querySelector(".submit-link");
 let form = document.querySelector("form");
 
+let firstName = document.querySelector("#first-name");
+let lastName = document.querySelector("#last-name");
+let names = document.querySelectorAll(".name");
+let email = document.querySelector("#email");
+let phone = document.querySelector("#phone");
+let password = document.querySelector("#password");
+let confirmPassword = document.querySelector("#confirm-password");
+let submitLink = document.querySelector(".submit-link");
+
+names.forEach(item => {
+    item.onkeydown = function() {
+        return /[a-z]/i.test(event.key);
+    }
+})
+
+
 submitLink.addEventListener("click", function() {
+
     form.submit();
 });
-
