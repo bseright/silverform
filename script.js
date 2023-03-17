559c5e390045ab28bc849ffc22b718aee4b9ba35
Britain Seright <51188389+bseright@users.noreply.github.com>
2023-03-16 19:03:30 -0500

    validate phone on submit

--- script.js
+++ script.js
@@ -2,13 +2,18 @@ let form = document.querySelector("form");
 
 let firstName = document.querySelector("#first-name");
 let lastName = document.querySelector("#last-name");
-let names = document.querySelectorAll(".name");
+
 let email = document.querySelector("#email");
 let emailLabel = document.querySelector("#email-label");
 let emailStatus;
+
 let phone = document.querySelector("#phone");
+let phoneLabel = document.querySelector("#phone-label");
+let phoneStatus;
+
 let password = document.querySelector("#password");
 let confirmPassword = document.querySelector("#confirm-password");
+
 let submitLink = document.querySelector(".submit-link");
 
 phone.onkeypress = function(event) {
@@ -22,9 +27,7 @@ phone.onkeypress = function(event) {
 }
 
 function validateEmail() {
-    let valid = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  
-    if (email.value.match(valid)) {
+    if (email.checkValidity()) {
         if (emailStatus === "invalid") {
             emailLabel.style.marginLeft = "0px";
             emailStatus = "valid";
@@ -39,9 +42,27 @@ function validateEmail() {
     }
 }
 
+function validatePhone() {
+    if (phone.checkValidity()) {
+        if (phoneStatus === "invalid") {
+            phoneLabel.style.marginLeft = "0px";
+            phoneStatus = "valid";
+            phoneLabel.textContent = "Phone";
+            phone.style.borderColor = "";
+        }
+    } else {
+        phoneLabel.style.marginLeft = "-5px";
+        phoneStatus = "invalid";
+        phoneLabel.textContent = "Invalid Phone";
+        phone.style.borderColor = "rgb(245, 132, 132)";
+    }
+}
+
+
 submitLink.addEventListener("click", function() {
 
     validateEmail();
+    validatePhone();
 
     // form.submit();
 });
